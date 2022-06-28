f12152d04ddb005ba2d2a6a2a5fc33d6066c2927
marcelo <marcelo@contassconsultoria.com.br>
2022-06-28 18:46:10 -0300

    Evento de domínio

--- src/Aplicacao/Aluno/MatricularAluno/MatricularAluno.php
+++ src/Aplicacao/Aluno/MatricularAluno/MatricularAluno.php
@@ -3,7 +3,10 @@
 namespace Alura\Arquitetura\Aplicacao\Aluno\MatricularAluno;
 
 use Alura\Arquitetura\Dominio\Aluno\Aluno;
+use Alura\Arquitetura\Dominio\Aluno\AlunoMatriculado;
+use Alura\Arquitetura\Dominio\Aluno\LogDeAlunoMatriculado;
 use Alura\Arquitetura\Dominio\Aluno\RepositorioDeAluno;
+use Alura\Arquitetura\Dominio\PublicadorDeEvento;
 
 class MatricularAluno
 {
@@ -12,11 +15,16 @@ class MatricularAluno
     public function __construct(RepositorioDeAluno $repositorioDeAluno)
     {
         $this->repositorioDeAluno = $repositorioDeAluno;
+        $this->publicador = new PublicadorDeEvento();
+        $this->publicador->adicionarOuvinte(new LogDeAlunoMatriculado());
     }
 
     public function executa(MatricularAlunoDto $dados): void
     {
         $aluno = Aluno::comCpfNomeEEmail($dados->cpfAluno, $dados->nomeAluno, $dados->emailAluno);
         $this->repositorioDeAluno->adicionar($aluno);
+
+        $evento = new AlunoMatriculado($aluno->cpf());
+        $this->publicador->publicar($evento);
     }
 }
---
+++ src/Dominio/Aluno/AlunoMatriculado.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Alura\Arquitetura\Dominio\Aluno;
+
+use Alura\Arquitetura\Dominio\Cpf;
+use Alura\Arquitetura\Dominio\Evento;
+
+class AlunoMatriculado implements Evento
+{
+    private \DateTimeImmutable $momento;
+
+    public function __construct(Cpf $cpfAluno)
+    {
+        $this->momento = new \DateTimeImmutable();
+        $this->cpfAluno = $cpfAluno;
+    }
+
+    public function cpfAluno(): Cpf
+    {
+        return $this->cpfAluno;
+    }
+
+    public function momento(): \DateTimeImmutable
+    {
+        return $this->momento;
+    }
+}
